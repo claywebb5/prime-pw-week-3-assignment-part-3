74822cc251124653c1231959e47ae14999cf6cb3
Clay Webb <clay_webb@outlook.com>
2021-12-16 16:16:31 -0600

    Style.css File Completed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -88,3 +88,16 @@ console.log(sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let parts = 1;
+const maxParts = 572;
+const box = 7;
+
+while( parts === box ){
+  parts++;
+  console.log('One box');
+
+}
+
+
+//
