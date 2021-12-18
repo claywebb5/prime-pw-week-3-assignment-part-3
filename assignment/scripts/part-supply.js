df5218897d7d33b090808b98a2936d84c3fec07d
Clay Webb <clay_webb@outlook.com>
2021-12-18 13:07:16 -0600

    Completed Assignment

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -88,17 +88,3 @@ console.log(sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
-
-let parts = 0;
-let boxes = 1;
-
-while( parts < 573 ){
-  parts++;
-  if ( parts === 7){
-    console.log('YES!');
-  }
-}
-
-
-
-//
