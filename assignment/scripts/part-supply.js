a9c1687c7dae7dec732f700e0005d590d303d08d
Clay Webb <clay_webb@outlook.com>
2021-12-30 20:50:08 -0600

    Updated Question 4 Per Graded Notes

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -24,8 +24,9 @@ console.log(supplyChanges[1]);
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
-supplyChanges.pop();
-  console.log('11');
+let removedValue = supplyChanges.pop();
+  console.log('Removed value is', removedValue);
+
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
